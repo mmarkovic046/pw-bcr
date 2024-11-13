7f487d8b613e4773a15b7e203da42cbe91ce67c0
m.markovic <m.markovic@greco.services>
2024-11-13 09:31:01 +0100

    added new comment

--- src/tests/createCascoOffer.spec.ts
+++ src/tests/createCascoOffer.spec.ts
@@ -20,7 +20,7 @@ test.describe("CASCO Offer", () => {
     console.log("URL:", baseURL);
     console.log("E-mail:", user.email);
   });
-
+  //First test
   test("Create CASCO offer (Individual)", async ({ page }) => {
     await new PageObjects(page).newOfferCascoPage();
     await new GeneralPageCasco(page).generalPageIndividual();
